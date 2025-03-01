64a7ab35dbb3d753fd18039e0f2cb271bf555f55
Blemet Dev <julien.deviller@outlook.com>
2025-03-01 14:56:49 +0100

    [ADD] Home button on personal monkey pages

--- src/pages/MonkeyPage.jsx
+++ src/pages/MonkeyPage.jsx
@@ -1,9 +1,10 @@
 import monkeys from '@constants/monkeys'
 import { Box, Button, Container, Typography } from '@mui/material'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const MonkeyPage = () => {
   const { id } = useParams()
+  const navigate = useNavigate()
   const monkey = monkeys.find((m) => m.id === parseInt(id))
 
   if (!monkey) {
@@ -15,69 +16,88 @@ const MonkeyPage = () => {
   }
 
   return (
-    <Container
-      sx={{
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        px: 5,
-      }}
-    >
-      <Box
-        component="img"
-        src={monkey.img}
-        alt={monkey.name}
-        sx={{
-          width: { xs: '35%', md: '38%' },
-          borderRadius: 2,
-          mb: 3,
-        }}
-      />
-
-      <Typography
-        variant="h3"
-        sx={{
-          fontWeight: 'bold',
-          mb: 2,
-          fontSize: { xs: '1.8rem', sm: '2rem', md: '3rem' },
-        }}
-      >
-        {monkey.name}
-      </Typography>
-
-      <Typography
-        variant="h5"
-        sx={{
-          color: 'green',
-          mb: 2,
-          fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' },
-        }}
-      >
-        {monkey.price}
-      </Typography>
-
-      <Typography
-        variant="body1"
-        sx={{
-          maxWidth: '600px',
-          fontSize: { xs: '0.9rem', sm: '1rem', md: '1.2rem' },
-          mb: 3,
-        }}
-      >
-        {monkey.description}
-      </Typography>
-
+    <Box sx={{ width: '100vw', minHeight: '100vh', position: 'relative' }}>
       <Button
         variant="contained"
-        color="primary"
-        sx={{ fontSize: '1rem', px: 4 }}
+        color="secondary"
+        onClick={() => navigate('/')}
+        sx={{
+          position: 'absolute',
+          top: 30,
+          left: 30,
+          fontSize: { xs: '0.8rem', md: '1rem' },
+          padding: { xs: '6px 12px', md: '8px 16px' },
+          zIndex: 10,
+        }}
       >
-        Buy Now
+        Home
       </Button>
-    </Container>
+      <Container
+        disableGutters
+        sx={{
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          px: 5,
+          position: 'relative',
+        }}
+      >
+        <Box
+          component="img"
+          src={monkey.img}
+          alt={monkey.name}
+          sx={{
+            width: { xs: '35%', md: '38%' },
+            borderRadius: 2,
+            mb: 3,
+          }}
+        />
+
+        <Typography
+          variant="h3"
+          sx={{
+            fontWeight: 'bold',
+            mb: 2,
+            fontSize: { xs: '1.8rem', sm: '2rem', md: '3rem' },
+          }}
+        >
+          {monkey.name}
+        </Typography>
+
+        <Typography
+          variant="h5"
+          sx={{
+            color: 'green',
+            mb: 2,
+            fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' },
+          }}
+        >
+          {monkey.price}
+        </Typography>
+
+        <Typography
+          variant="body1"
+          sx={{
+            maxWidth: '600px',
+            fontSize: { xs: '0.9rem', sm: '1rem', md: '1.2rem' },
+            mb: 3,
+          }}
+        >
+          {monkey.description}
+        </Typography>
+
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ fontSize: '1rem', px: 4 }}
+        >
+          Buy Now
+        </Button>
+      </Container>
+    </Box>
   )
 }
 
