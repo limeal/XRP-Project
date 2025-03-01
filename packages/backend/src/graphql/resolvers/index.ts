3b35596d65aca8146828f367387c23c5d2e950de
Paul <paul.gazeau-rousseau@epitech.eu>
2025-03-01 17:18:05 +0100

    feat(backend): setup prisma
    Refs: BLO-15

--- packages/backend/src/graphql/resolvers/index.ts
+++ packages/backend/src/graphql/resolvers/index.ts
@@ -1,31 +1,38 @@
+import prisma from '../../prisma/client';
+
 const resolvers = {
   Query: {
     users: async () => {
-      return [
-        {
-          id: '1',
-          email: 'test@test.com',
-          name: 'Test',
-        },
-      ];
+      return await prisma.user.findMany();
     },
     user: async (_: any, { id }: { id: string }) => {
-      return {
-        id: '1',
-        email: 'test@test.com',
-        name: 'Test',
-      };
+      return await prisma.user.findUnique({
+        where: { id },
+      });
     },
   },
   Mutation: {
     createUser: async (_: any, { email, name }: { email: string; name?: string }) => {
-     console.log('createUser', email, name);
+      return await prisma.user.create({
+        data: {
+          email,
+          name,
+        },
+      });
     },
     updateUser: async (_: any, { id, email, name }: { id: string; email?: string; name?: string }) => {
-        console.log('updateUser', id, email, name);
+      return await prisma.user.update({
+        where: { id },
+        data: {
+          ...(email && { email }),
+          ...(name && { name }),
+        },
+      });
     },
     deleteUser: async (_: any, { id }: { id: string }) => {
-        console.log('deleteUser', id);
+      return await prisma.user.delete({
+        where: { id },
+      });
     },
   },
 };
